e2e916ef6cf01fb3d8b91110cc8e09eec8a60684
Irungu Macharia <machariajosphat001@gmail.com>
2024-08-15 09:25:23 +0000

    some fixes -db etc

--- client/src/constants/baseurl.js
+++ client/src/constants/baseurl.js
@@ -5,7 +5,8 @@ exports.BASE_API_ROUTE =
     ? "http://localhost:5000"
     : "https://example.com";
 
-exports.LOGIN_ROUTE = `${this.BASE_API_ROUTE}/user/login`;
+//exports.LOGIN_ROUTE = `${this.BASE_API_ROUTE}/user/login`;
+exports.LOGIN_ROUTE = `${this.BASE_API_ROUTE}/login`;
 
 exports.SIGN_UP_ROUTES = {
   ind: `${this.BASE_API_ROUTE}/sign_up`,
